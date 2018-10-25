f9216be3fbae2e84e28a7ac3a15d184be70a04b3
Prandar <prangere.romain@gmail.com>
2018-10-25 11:17:32 +0200

    ajout des param MainWindow

--- GameList3.0/MainWindow.xaml.cs
+++ GameList3.0/MainWindow.xaml.cs
@@ -21,9 +21,31 @@ namespace GameListWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Utilisateur> listUsers = new List<Utilisateur>();
+
+        public void RemplirListUser()
+        {
+            Utilisateur UserYohan = new Utilisateur(0, "Yohan", "Admin", "yohan.angelini@epsi.fr");
+            Utilisateur UserRomain = new Utilisateur(0, "Romain", "password", "romain.prangere@epsi.fr");
+            listUsers.Add(UserYohan);
+            listUsers.Add(UserRomain);
+        }
+        public int Verif(List<Utilisateur> _listUser, string _login, string _mdp)
+        {
+            foreach (Utilisateur _user in _listUser)
+            {
+                if (_user.Login == _login && _user.Mdp == _mdp)
+                {
+                    return _user.Id;
+                }
+            }
+            return -1;
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void 
     }
 }
